be7049ce0949d44be81bc1bb1d85da1f82297d6b
Radhey Chitroda <radheychitroda@gmail.com>
2023-03-06 17:44:42 +0530

    Changes in theme button

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const themeBtn = document.querySelector('.theme-btn')
+const body = document.body
 
 themeBtn.addEventListener('click',()=>{
+    const i = document.createElement('i')
     themeBtn.classList.toggle('light')
-    document.body.classList.toggle('light')
+    body.classList.toggle('light')
+    themeBtn.innerHTML = ""
+    if (body.classList.contains('light')) {
+        i.classList.add('fa-regular')
+        i.classList.add('fa-lightbulb')
+    }
+    else {
+        i.classList.add('fa-solid')
+        i.classList.add('fa-lightbulb')
+    }
+    themeBtn.appendChild(i)
 })
